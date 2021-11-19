08307cceb7360f2265bde7087a60f26584d19fb0
Fauzan Rifki Maulana <80396521+frmdeveloper@users.noreply.github.com>
2021-11-19 21:01:44 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-var express = require('express'),
-    cors = require('cors'),
-    secure = require('ssl-express-www');
+var express = require('express')
 const PORT = process.env.PORT || 8080 || 5000 || 3000
 var app = express()
 const apiii = require("./api/api")
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(secure)
-app.use(cors())
 app.use("/", apiii);
 app.listen(PORT, async () => {
 	console.log(`Server berjalan dengan port: ${PORT}\n`)
