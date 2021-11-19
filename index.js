c6cce2959031c235b9e3972087c6c0fc9408dced
Fauzan Rifki Maulana <80396521+frmdeveloper@users.noreply.github.com>
2021-11-20 05:45:11 +0700

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.get('/', async (req, res) => {
+	var gethtml = await axios.get("https://frmdeveloper.github.io/countapi"+req.url)
+	res.set("content-type",gethtml.headers['content-type']).send(gethtml.data)
+})
 app.get('/about', (req, res) => res.send('About Page Route'));
 app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 app.get('/contact', (req, res) => res.send('Contact Page Route'));
